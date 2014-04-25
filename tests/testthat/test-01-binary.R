b8f1b3868d9e2cfd857280d90310e202253c147b
Sebastian Kopf <skopf@caltech.edu>
2014-04-25 15:10:01 -0700

    test renaming
    so they run in order

--- tests/testthat/test-01-binary.R
+++ tests/testthat/test-01-binary.R
@@ -1,5 +1,9 @@
 context("BinaryFile")
 
+test_that("Testing External Data Availability", {
+  expect_that(length(system.file("extdata", "6520__F8-5_5uL_isodat2.cf", package="isoread")) > 0, is_true())
+})
+
 test_that("Testing BinaryFile Class", {
   
   # load
