98147fe4356d3e35dcda65a19c3818c2508f2287
Sebastian Kopf <skopf@caltech.edu>
2014-04-30 12:32:38 -0700

    onload fix
    remove automatic object creation

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 #FIXME remove again
 .onAttach <- function(lib, pgk) {
-  isoread("/Users/sk/Dropbox/VM Windows/6520__F8-5_5uL_isodat2.cf", readChromData = TRUE, type = "H_CSIA") ->> i
+  #isoread("/Users/sk/Dropbox/VM Windows/6520__F8-5_5uL_isodat2.cf", readChromData = TRUE, type = "H_CSIA") ->> i
 }
 
 # this is not executed automatically in autotesting (since autotesting only 
